d129bbfabd7dc76e99763845060fa5032799512e
leutnant <karl.shtolz.army@gmail.com>
2023-05-26 19:45:19 +0300

    AdvancedFeatures

---
+++ src/d_input_output_file/advanced_features/AdvancedFeatures_5_4_8.java
@@ -0,0 +1,18 @@
+public class AdvancedFeatures_5_4_8 {
+
+}
+class Animal implements Serializable {
+    private final String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Animal) {
+            return Objects.equals(name, ((Animal) obj).name);
+        }
+        return false;
+    }
+}
