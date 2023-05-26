45b7870cf0a42dd7518b430390e116b3afb451b5
leutnant <karl.shtolz.army@gmail.com>
2023-05-26 19:24:32 +0300

    ByteStream

---
+++ src/d_input_output_file/byte_stream/ByteStream.java
@@ -0,0 +1,16 @@
+public class ByteStream {
+    public static int checkSumOfStream(InputStream inputStream) throws IOException {
+        int b;
+        int crc = 0;
+        try {
+            while ((b = inputStream.read()) != -1) {
+                crc = Integer.rotateLeft(crc, 1) ^ b;
+            }
+        } catch (IOException e) {
+            throw new IOException();
+        }
+
+        return crc;
+    }
+
+}
